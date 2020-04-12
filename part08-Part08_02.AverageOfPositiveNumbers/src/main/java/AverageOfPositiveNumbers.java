8589060870420caccd02be9fe8975ae70bf18210
Michael Tran <michael.tran2208@gmail.com>
2020-04-12 18:34:53 +1000

    finished part 8 section 1: recap

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,29 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int sum = 0;
+        int count = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("0")) {
+                break;
+            }
+            
+            int value = Integer.valueOf(input);
+            if (value > 0) {
+                sum += value;
+                count++;
+            }
+        }
+        
+        if (count != 0) {
+            System.out.println(1.0*sum/count);
+        } else {
+            System.out.println("Cannot calculate the average");
+        }
 
     }
 }
---
+++ part08-Part08_04.LiquidContainers2/src/main/java/Container.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author michael
+ */
+public class Container {
+    
+    private int volume;
+
+    public Container() {
+        this.volume = 0;
+    }
+    
+    public int contains() {
+        return this.volume;
+    }
+    
+    public void add(int amount) {
+        if (amount < 0) {
+            return;
+        }
+        
+        this.volume += amount;
+        if (this.volume > 100) {
+            this.volume =100;
+        }
+    }
+    
+    public void remove(int amount) {
+        if (amount < 0) {
+            return;
+        }
+        this.volume -= amount;
+        if (this.volume < 0) {
+            this.volume = 0;
+        }
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+    
+    
+    
+    @Override
+    public String toString() {
+        return this.contains() + "/100";
+    }
+
+}
